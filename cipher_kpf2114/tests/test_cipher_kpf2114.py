b1547bec54c828824a178854a9910b7671210be1
Kevin Frazier <90807489+kpf2114@users.noreply.github.com>
2021-11-09 01:57:38 -0500

    Update test_cipher_kpf2114.py

--- cipher_kpf2114/tests/test_cipher_kpf2114.py
+++ cipher_kpf2114/tests/test_cipher_kpf2114.py
@@ -1,4 +1,18 @@
 from cipher_kpf2114 import cipher_kpf2114
+def cipher(text, shift, encrypt=True):
+    assert isinstance(shift, str) == False, "Cannot use strings for shift values"
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    new_text = ''
+    for c in text:
+        index = alphabet.find(c)
+        if index == -1:
+            new_text += c
+        else:
+            new_index = index + shift if encrypt == True else index - shift
+            new_index %= len(alphabet)
+            new_text += alphabet[new_index:new_index+1]
+    return new_text
+
 def test_cipher_negshift():
     example = 'weekend'
     expected = 'WEEKEND'
