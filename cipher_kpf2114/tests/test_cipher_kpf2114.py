3fb7f91619e7f429a10edb6ea74e958a9f6b30d0
Kevin Frazier <90807489+kpf2114@users.noreply.github.com>
2021-11-08 23:42:54 -0500

    Update test_cipher_kpf2114.py

--- cipher_kpf2114/tests/test_cipher_kpf2114.py
+++ cipher_kpf2114/tests/test_cipher_kpf2114.py
@@ -1,1 +1,6 @@
 from cipher_kpf2114 import cipher_kpf2114
+def test_cipher_negshift():
+    example = 'weekend'
+    expected = 'WEEKEND'
+    actual = cipher(example, -26)
+    assert actual == expected
