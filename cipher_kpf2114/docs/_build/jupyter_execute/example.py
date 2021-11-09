cd90f6b080e5a488d457d7e94a78c624c53a6b18
Kevin <kpf2114@columbia.edu>
2021-11-08 23:30:24 -0500

    docs/_build/

---
+++ cipher_kpf2114/docs/_build/jupyter_execute/example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Example usage
+# 
+# To use `cipher_kpf2114` in a project:
+
+# In[1]:
+
+
+import cipher_kpf2114
+
+print(cipher_kpf2114.__version__)
+
